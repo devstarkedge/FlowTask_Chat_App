ef44205ec1f4cab386282553957bc8cf5ae8f0ca
Tisha <dev@starkedge.com>
2026-07-09 15:32:54 +0530

    Changes in Sanitze file

--- server/utils/sanitize.js
+++ server/utils/sanitize.js
@@ -13,7 +13,6 @@ const ALLOWED_TAGS = [
   'p', 'br', 'strong', 'em', 'u', 's', 'code', 'pre',
   'blockquote', 'ul', 'ol', 'li', 'a', 'span',
   'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
-  'div', 'b', 'i', 'strike', 'del',
 ];
 
 const ALLOWED_ATTRS = ['href', 'target', 'rel', 'class', 'data-mention-id', 'data-mention-type'];
